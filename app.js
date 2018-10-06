774a88486e66b23b0e382f76a7dad095cf8ac84b
dnzengou <desire.yavro@gmail.com>
2018-10-06 20:34:45 +0200

    update files

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
-var $input = $('input');
-var $LastName = $('#Name');
+// Setting elements
+var es = {}; // Elements array
+['output',
+  'input'
+].forEach(function(element, index, array) {
+  es[element] = document.getElementById(element);
+})
 
+// Style stuff
+input.onfocus = function() {
+  input.classList.add("in");
+  input.classList.remove("out");
+}
+input.onblur = function() {
+  input.classList.remove("in");
+  input.classList.add("out");
+}
 
-$(document).ready(function() {
-    var client = algoliasearch('', '');
-    var index = client.initIndex('Document_information');
+// Algolia login stuff
+var client = algoliasearch("KC17SSROI6", "6fabfd6313a9783bf11ce9005ceadfc3");
+var index = client.initIndex('getstarted_actors');
 
-    $input.keyup(function() {
-        index.search($input.val(), {
-            hitsPerPage: 10,
-            facets: '*'
-        }, searchCallback);
-    }).focus();
-});
-
-function searchCallback(err, content) {
-    if (err) {
+// Search
+es['input'].onkeyup = function() {
+  if (es['input'].value.length) {
+    index.search(es['input'].value, function searchDone(err, content) {
+      if (err) {
         console.error(err);
         return;
-    }
+      }
 
-    $Name.empty();
-
-    for (var i = 0; i < content.hits.length; i++) {
-        $Name.append('<li>' + ' ' + '<b>' + ' | ' + 'ID ' + '<i>' + content.hits[i].No + '</i>' + ', ' + '</li>');
-    }
-};
+      if(content && content.hits.length) {
+        var list = content.hits.map(function(e,i,a) {
+           return "<li>" + e._highlightResult.name.value + " - " + e.rating + '</li>';
+        }).join('');
+        es['output'].innerHTML = "<h5>Completed in " + content.processingTimeMS + "ms</h5><ul>" + list + '</ul>';
+      } else {
+        es['output'].innerHTML = "<h5>No matches found</h5>";
+      }
+    })
+  } else {
+    es['output'].innerHTML = "";
+  }
+}
